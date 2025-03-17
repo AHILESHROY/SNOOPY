47b8d3be9ed023ceb5c0e764ba4497f5614e4b5f
AHILESHROY <ahileshroy007@gmail.com>
2025-03-17 16:05:16 +0530

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
-import React from 'react';
-import LoginForm from './components/LoginForm.jsx';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginForm from "./components/LoginForm.jsx";
+import Home from "./components/HomePage/HomePage.jsx"; 
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/home" element={<Home/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
